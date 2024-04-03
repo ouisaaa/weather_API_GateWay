9ab77475be42ac34f2f4c1966531dd67c1d8eee7
ouisaaa_pc <ouisaaa@gmail.com>
2024-04-03 23:58:55 +0900

    0403

---
+++ config.py
@@ -0,0 +1,4 @@
+
+
+class Config:
+    DATA_DOMAIN="http://localhost"
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+from config import Config
 
 app = FastAPI()
+config=Config()
 
 #
 # @app.get("/")
@@ -16,20 +17,51 @@ import asyncio
 @app.get("/getCurrent")
 async def request_location(x: float=130,y: float=63):
     async with httpx.AsyncClient() as client:
-        response = await client.get('http://localhost:3030/location?x={x}&y={y}')
+        response = await client.get(f'{config.DATA_DOMAIN}:4001/location?x={x}&y={y}')
         return response.json()
 
 @app.get("/getSrtFcst")
 async def request_SrtFcst(city: str,district: str,neighborhood: str):
-    response = await client.get(f'http://localhost:4001/weather/getUltraSrtFcst?city={city}&district={district}&neighborhood={neighborhood}')
-    return response.json()
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f'{config.DATA_DOMAIN}:4002/weather/getUltraSrtFcst?city={city}&district={district}&neighborhood={neighborhood}')
+        return response.json()
 
 @app.get("/getSrtNcst")
 async def request_SrtFcst(city: str,district: str,neighborhood: str):
-    response = await client.get(f'http://localhost:4002/weather/getSrtNcst?city={city}&district={district}&neighborhood={neighborhood}')
-    return response.json()
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f'{config.DATA_DOMAIN}:4003/weather/getSrtNcst?city={city}&district={district}&neighborhood={neighborhood}')
+        return response.json()
 
 @app.get("/getVliageFcst")
 async def request_VliageFcst(city: str,district: str,neighborhood: str):
-    response = await client.get(f'http://localhost:4003/weather/getVliageFcst?city={city}&district={district}&neighborhood={neighborhood}')
-    return response.json()
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f'{config.DATA_DOMAIN}:4004/weather/getVliageFcst?city={city}&district={district}&neighborhood={neighborhood}')
+        return response.json()
+
+@app.get("/districtList")
+async def request_dis(city: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f'{config.DATA_DOMAIN}:4005/weather/districtList?city={city}')
+        temp=response.text[1:-1].split(", ") #리스트 형태로 데이터를 전송
+        temp[0]="-"
+        return temp
+
+@app.get("/neighborhoodList")
+async def request_nei(dis: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f'{config.DATA_DOMAIN}:4005/weather/neighborhoodList?district={dis}')
+        temp = response.text[1:-1].split(", ")  # 리스트 형태로 데이터를 전송
+        temp[0] = "-"
+        return temp
+
+@app.get("/CtprvnRltmMesureDnsty")
+async def request_CRMD(city: str, nei: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f'{config.DATA_DOMAIN}:4006/CtprvnRltmMesureDnsty?city={city}&nei={nei}')
+        return response.json()
+
+@app.get("/CtprvnRltmMesureDnsty/total")
+async def request_CRMD_total(city: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f'{config.DATA_DOMAIN}:4006/CtprvnRltmMesureDnsty/total?city={city}')
+        return response.json()
